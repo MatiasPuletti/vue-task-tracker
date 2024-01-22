390ee84914278a038c03fa9649e321710ef88e13
Matias Puletti <matiaspuletti@Matiass-Laptop.local>
2024-01-22 21:52:17 +0100

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button />
+  </header>
+</template>
+
+<script>
+import Button from "./Button";
+
+export default {
+  name: "Header",
+  props: {
+    title: String,
+  },
+  components: {
+    Button,
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
