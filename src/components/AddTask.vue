e1a6ec6379e8e77946386b5d7f7e9b032fa36937
Matias Puletti <matiaspuletti@Matiass-Laptop.local>
2024-01-27 18:04:53 +0100

    Implement AddTask form

---
+++ src/components/AddTask.vue
@@ -0,0 +1,23 @@
+<template>
+  <form class="add-from">
+    <div class="form-control">
+      <label>Task</label>
+      <input type="text" name="text" placeholder="Add Task" />
+    </div>
+    <div class="form-comtrol">
+      <label>Day & Time</label>
+      <input type="text" name="text" placeholder="Add Day & Time" />
+    </div>
+    <div class="form-control form-control-check">
+      <label>Set Reminder</label>
+      <input type="checkbox" name="reminder" />
+    </div>
+    <input type="submit" value="Save task" class="bt n btn-block" />
+  </form>
+</template>
+
+<script>
+export default {
+  name: "AddTask",
+};
+</script>
